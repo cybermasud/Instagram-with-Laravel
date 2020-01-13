3b20d8c6f7ca8dedf03a7d4c36a33fdc110175f7
masood <m.amiri.t@gmail.com>
2020-01-13 17:37:03 +0330

    added post relations with media and user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,8 @@
                 @auth
                     <div class="nav-item">
                         <a class="text-decoration-none text-dark"
-                           href="{{route('profile.show', $user->username)}}">
-                            {{ $user->username }}
+                           href="{{route('profile.show',\Illuminate\Support\Facades\Auth::user()->username)}}">
+                            {{ Auth::user()->username }}
                         </a>
                     </div>
                 @endauth
